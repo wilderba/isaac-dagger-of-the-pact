9e90c706e8dc6298c6028416e7b8d7c6d725e19a
Ben Wilder <wilderba@miamioh.edu>
2017-12-28 20:24:23 -0500

    Altered blue spider gains from RewardAllies rank 5

--- main.lua
+++ main.lua
@@ -185,7 +185,7 @@ function RewardAllies()
 		AddTempAlly()
 		AddTempAlly()
 	else
-		for i = 1, 12 do
+		for i = 1, 10 do
 			player:AddBlueSpider(player.Position)
 		end
 		player:AddBlueFlies(8, player.Position, player)
